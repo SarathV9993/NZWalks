33310093d22caa776b1805c53f9a79e532cc650c
SarathV9993 <sarath.vittanala34@gmail.com>
2022-08-21 23:17:05 +0530

    End of Section 2

---
+++ NzWalks/NZWalks.API/Data/NzWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NzWalksDbContext : DbContext
+    {
+        public NzWalksDbContext(DbContextOptions<NzWalksDbContext> options): base(options)
+        {
+
+        }
+        // By creating properties like this we are telling Entity Framework that 
+        // if a table doesn't exists then please create a table.
+        // This is a connection and a property that tells you that you can
+        // talk to Regions table
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
