e997988a096500556f773345d172e251c548d32a
SarathV9993 <sarath.vittanala34@gmail.com>
2022-08-29 11:40:01 +0530

    End of Section 6

---
+++ NzWalks/NZWalks.API/Repositories/WalkDifficultyRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+
+namespace NZWalks.API.Repositories
+{
+    public class WalkDifficultyRepository : IWalkDifficultyRepository
+    {
+        private readonly NzWalksDbContext nzWalksDbContext;
+
+        public WalkDifficultyRepository(NzWalksDbContext nzWalksDbContext)
+        {
+            this.nzWalksDbContext = nzWalksDbContext;
+        }
+
+        public async Task<WalkDifficulty> AddAsync(WalkDifficulty walkDifficulty)
+        {
+            walkDifficulty.Id = Guid.NewGuid();
+            await nzWalksDbContext.WalkDifficulty.AddAsync(walkDifficulty);
+            await nzWalksDbContext.SaveChangesAsync();
+            return walkDifficulty;
+
+        }
+
+        public async Task<WalkDifficulty> DeleteAsync(Guid id)
+        {
+            var existingWalkDifficulty = await nzWalksDbContext.WalkDifficulty.FindAsync(id);
+            if(existingWalkDifficulty != null)
+            {
+                nzWalksDbContext.WalkDifficulty.Remove(existingWalkDifficulty);
+                await nzWalksDbContext.SaveChangesAsync();
+                return existingWalkDifficulty;
+            }
+            return null;
+        }
+
+        public async Task<IEnumerable<WalkDifficulty>> GetAllAsync()
+        {
+            return await nzWalksDbContext.WalkDifficulty.ToListAsync();
+        }
+
+        public async Task<WalkDifficulty> GetAsync(Guid id)
+        {
+            return await nzWalksDbContext.WalkDifficulty.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<WalkDifficulty> UpdateAsync(Guid id, WalkDifficulty walkDifficulty)
+        {
+            var existingWalkDifficulty = await nzWalksDbContext.WalkDifficulty.FindAsync(id);
+
+            if (existingWalkDifficulty == null)
+            {
+                return null;
+            }
+
+            existingWalkDifficulty.Code = walkDifficulty.Code;
+            await nzWalksDbContext.SaveChangesAsync();
+            return existingWalkDifficulty;
+        }
+    }
+}
